e5302f8d79d528ac0dfa4d25b48146fed2db908e
Alex <alexvasvas78@gmail.com>
2024-08-21 17:33:49 +0300

    del list

--- module_5_1.py
+++ module_5_1.py
@@ -4,17 +4,13 @@ class House:
         self.number_of_floors = floors
 
     def go_to(self, new_floor):
-        floor = int(new_floor)
-        floors = list(range(1, floor + 1))
-        print(floors)
-        if floor > self.number_of_floors or floor < 1:
-            print("Такого этажа не существует")
 
-        # if 1 <= new_floor <= self.number_of_floors:
-        #     for floor in range(1, new_floor + 1):
-        #         print(floor)
-        #     else:
-        #         print("Такого этажа не существует")
+        if 1 <= new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+            else:
+                print("Такого этажа не существует")
+
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
