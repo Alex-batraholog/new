2252b9b671895bfe4f73aa02cc3ed16db2f3ff2a
Alex <alexvasvas78@gmail.com>
2024-08-21 09:31:47 +0300

    delite self

--- module_5_1.py
+++ module_5_1.py
@@ -1,8 +1,7 @@
 class House:
-    def __init__(self, name, number_of_floors):
+    def __init__(self, name, floors):
         self.name = name
-        self.number_of_floors = number_of_floors
-        self.go_to(self)
+        self.number_of_floors = floors
 
     def go_to(self, new_floor):
         floor = int(new_floor)
@@ -11,6 +10,11 @@ class House:
         if floor > self.number_of_floors or floor < 1:
             print("Такого этажа не существует")
 
+        # if 1 <= new_floor <= self.number_of_floors:
+        #     for floor in range(1, new_floor + 1):
+        #         print(floor)
+        #     else:
+        #         print("Такого этажа не существует")
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
