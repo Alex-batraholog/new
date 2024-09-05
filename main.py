8cd39205ce4295f849ed0d5443eaf2a5e28588c7
Alex <alexvasvas78@gmail.com>
2024-09-05 23:19:50 +0300

    return

--- main.py
+++ main.py
@@ -1,52 +1,38 @@
-# list
-# range(3,21)
-# result = n %
-# print(result)
-
-# numbers = (i for i in range(3, 21))
-# for number in numbers:
-#     if i
-#     print(number)
-
-
-class House:
-    def __init__(self, name, number_of_floors):
+class Human:
+    def __init__(self,name,age):
         self.name = name
-        self.number_of_floors = number_of_floors
-        self.go_to()
-
-
-    def go_to(new_floor):
-        floor = int(new_floor)
-        floors = list(range(1, floor + 1))
-        print(floors)
-        if floor > self.number_of_floors or floor < 1:
-            print("Такого этажа не существует")
-
-
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
-
-print(h1.name, h1.number_of_floors)
-print(h2.name, h2.number_of_floors)
-
-
-
-
-
-
-
-
-#
-# def __del__(self):
-#     print(f'{self.name} ушёл')
+        self.age = age
+        self.say_info()
 #
 #
-# den = Human('Денис',22)
-# max = Human('Макс',22)
-# del den
-# max.birthday()
-# input()
+    def say_info(self):
+        print(f'Привет, меня зовут {self.name},мне {self.age}')
+#
+#
+#
+    def birthday(self):
+       self.age += 1
+       print(f'У меня день рождения, мне теперь {self.age}')
+#
+#
+    def __len__(self):
+        return self.age
+#
+    def __del__(self):
+       print(f'{self.name} ушёл')
 
+
+den = Human('Денис',22)
+max = Human('Макс',22)
+
+# den.birthday()
+# den.surname = "Попов"
+# print(den.name, den.age)
+# print(max.name, max.age)
+# den.age = 23
+# print(den.name, den.age)
+# print(den.surname)
+print(len(den))
+del den
+max.birthday()
+#input()
