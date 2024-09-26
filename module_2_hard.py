064043614d8fb1bf9c7832c82314dc9291d8f56b
Alex <alexvasvas78@gmail.com>
2024-09-26 22:22:52 +0300

    com

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,15 +1,22 @@
 def generate_password(n):
     result = ""
-    for i in range(1, 21):
-        for j in range(i, 21):
-            pair_sum = i + j
 
-            if n % pair_sum == 0:
-                result += f"{i}{j}"
+    # Генерация уникальных пар
+    pairs = []
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            pairs.append((i, j))
+
+    # Проверка кратности и формирование пароля
+    for pair in pairs:
+        pair_sum = sum(pair)
+        if n % pair_sum == 0:  # Проверка кратности
+            result += f"{pair[0]}{pair[1]}"
 
     return result
 
 
-for number in range(3, 21):
-    password = generate_password(number)
-    print(f"{number} - {password}")
+# Пример использования:
+for num in range(3, 21):
+    password = generate_password(num)
+    print(f"{num} - {password}")
